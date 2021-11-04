f58d1a7fa326586fa69e47a243b44ee00d27a6c7
Miguel Ortiz <miguel.ortiz@ceiba.com.co>
2021-11-04 17:13:38 -0500

    correcciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  endpoint: '/prospectos-vida'
+  endpoint: '/cinemaa',
+  endpointPeliculas: 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9a803e13429cd8df9ee4b1c157f87151',
+  urlTrm: 'https://www.datos.gov.co/resource/ceyp-9c7c.json',
+  tokenTrm: 'JjZ6JrMR9SKo5CApGp2PX9KzJ'
 };
