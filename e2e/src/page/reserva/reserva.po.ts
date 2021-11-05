90db6b473184b54adbd03f3168c84fa42b23293c
Miguel Ortiz <miguel.ortiz@ceiba.com.co>
2021-11-05 10:19:10 -0500

    Correccion

---
+++ e2e/src/page/reserva/reserva.po.ts
@@ -0,0 +1,36 @@
+import { by, element } from 'protractor';
+
+export class ReservaPage {
+    private linkCrearReserva = element(by.id('crearReserva'));
+    private inputNombrePelicula = element(by.id('pelicula'));
+    private inputCodigo = element(by.id('codigo'));
+    private inputTipoCarro = element(by.id('tipoCarro'));
+    private inputFechaReserva = element(by.id('fechaReserva'));
+    private inputHoraReserva = element(by.id('horaReserva'));
+    private inputIdUsuario = element(by.id('idUsuario'));
+    
+    async clickBotonCrearReserva() {
+        await this.linkCrearReserva.click();
+    }
+
+    async ingresarPelicula(pelicula){
+        await this.inputNombrePelicula.sendKeys(pelicula);
+    }
+    async ingresarCodigo(codigo){
+        await this.inputCodigo.sendKeys(codigo);
+    }
+    async ingresarTipoCarro(tipoCarro){
+        await this.inputTipoCarro.sendKeys(tipoCarro);
+    }
+    async ingresarFechaReserva(fechaReserva){
+        await this.inputFechaReserva.sendKeys(fechaReserva);
+    }
+    async ingresarHoraReserva(horaReserva){
+        await this.inputHoraReserva.sendKeys(horaReserva);
+    }
+    async ingresarIdUsuario(idUsuario){
+        await this.inputIdUsuario.sendKeys(idUsuario);
+    }
+
+
+}
---
+++ e2e/src/test/reserva.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { AppPage } from '../app.po';
+import { NavbarPage } from '../page/navbar/navbar.po';
+import { ReservaPage } from '../page/reserva/reserva.po';
+
+
+describe('workspace-project Reserva', () => {
+    let page: AppPage;
+    let navBar: NavbarPage;
+    let reserva: ReservaPage;
+
+    beforeEach(() => {
+        page = new AppPage();
+        navBar = new NavbarPage();
+        reserva = new ReservaPage();
+    });
+
+    it('deberia crear una reserva', () => {
+        const PELICULA = 'prueba';
+        const CODIGO = 'abds';
+        const TIPO_CARRO = 'AUTOMOVIL';
+        const FECHA_RESERVA = '2021-11-10';
+        const HORA_RESERVA = '12:00:00';
+        const NOMBRE_USUARIO = 'miguel';
+
+        page.navigateTo();
+        navBar.clickBotonReservas();
+        reserva.clickBotonCrearReserva();
+        reserva.ingresarPelicula(PELICULA);
+        reserva.ingresarCodigo(CODIGO);
+        reserva.ingresarTipoCarro(TIPO_CARRO);
+        reserva.ingresarFechaReserva(FECHA_RESERVA);
+        reserva.ingresarHoraReserva(HORA_RESERVA);
+        reserva.ingresarIdUsuario(NOMBRE_USUARIO);
+    })
+})
