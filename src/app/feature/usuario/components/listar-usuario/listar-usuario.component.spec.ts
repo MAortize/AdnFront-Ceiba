0511fb83042442311cbfe5dbe97c19f2e755158b
Miguel Ortiz <miguel.ortiz@ceiba.com.co>
2021-11-05 00:13:35 -0500

    correcciones

--- src/app/feature/usuario/components/listar-usuario/listar-usuario.component.spec.ts
+++ src/app/feature/usuario/components/listar-usuario/listar-usuario.component.spec.ts
@@ -1,13 +1,20 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpService } from '@core/services/http.service';
+import { Usuario } from '@shared/model/usuario';
 import { UsuarioService } from '@usuario/shared/service/usuario.service';
+import { of } from 'rxjs';
 
 import { ListarUsuarioComponent } from './listar-usuario.component';
 
 describe('ListarUsuarioComponent', () => {
   let component: ListarUsuarioComponent;
   let fixture: ComponentFixture<ListarUsuarioComponent>;
+  let usuarioService: UsuarioService
+  const listUsuaios: Usuario[] = [
+    new Usuario('Miguel', 'ortiz.eche@gmail.com'),
+    new Usuario('Alejandro', 'xd.eche@gmail.com')
+  ]
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,6 +28,10 @@ describe('ListarUsuarioComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarUsuarioComponent);
     component = fixture.componentInstance;
+    usuarioService = TestBed.inject(UsuarioService);
+    spyOn(usuarioService, 'consultar').and.returnValue(
+      of(listUsuaios)
+    )
     fixture.detectChanges();
   });
 
